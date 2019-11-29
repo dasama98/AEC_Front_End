9a06a31273cc0d1bbff979f8765353412c74248b
dasama98 <davidgendron@hotmail.com>
2019-11-28 23:11:21 -0500

    update

--- Programmation1/ExamenFormatif/js/Question6.js
+++ Programmation1/ExamenFormatif/js/Question6.js
@@ -9,18 +9,18 @@ var LEDprix;
 var filamentprix =20;
 var batterieprix=15;
 var total;
-controleur = prompt("quel controleur voulez vous,le Raspberry Pi à 55$ ou le Raspberry pi zero à 15$ ");
+controleur =Number(prompt("choisissez le controleur que vous voulez tapez 1 pour le raspberry pie à 55$ ou 2 pour le raspberry pi zero" ));
 moteurs= Number(prompt("combien de moteurs avez vous besoin choissisez entre 1 et 24"));
-camera = prompt("voulez vous une camera USB a 35$? tapez oui ou non");
-LED=prompt("voulez vous une matrice de LED pour les yeux à 10$ tapez oui ou non");
+camera = Number(prompt("voulez vous une camera USB a 35$? tapez 1 pour oui ou 2 pour non"));
+LED=Number(prompt("voulez vous une matrice de LED pour les yeux à 10$ tapez 1 pour oui ou 2 pour non"));
 
 
-if(controleur.toLowerCase() === "raspberry pi"){
+if(controleur === "1"){
     controleurprix=55;
     document.write("raspberry pi");
     document.write("<br/>")
 }
-    else if(controleur.toLowerCase()=== "raspberry pi zero"){
+    else if(controleur=== "2"){
         controleurprix=15;
     document.write("raspberry pi zero");
     document.write("<br/>")
@@ -31,20 +31,20 @@ if(moteurs >=2 && moteurs<= 24){
     document.write(moteurs + " moteurs");
     document.write("<br/>")
 }
-if(camera.toLowerCase() === "oui"){
+if(camera === "1"){
     cameraprix=35;
     document.write("camera");
     document.write("<br/>")
 }
-    else if(camera.toLowerCase() === "non"){
+    else if(camera=== "2"){
         cameraprix=0
 }
-if(LED.toLowerCase() ==="oui"){
+if(LED.toLowerCase() ==="1"){
     LEDprix=10;
     document.write("LED");
     document.write("<br/>")
 }
-    else if(LED === "non"){
+    else if(LED === "2"){
         LEDprix=0
 }
 document.write("filament3D");
