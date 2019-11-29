740cf9d9d312122903d167906548183d964c39ca
dasama98 <davidgendron@hotmail.com>
2019-11-28 23:20:44 -0500

    update3

--- Programmation1/ExamenFormatif/js/Question6.js
+++ Programmation1/ExamenFormatif/js/Question6.js
@@ -10,7 +10,7 @@ var filamentprix =20;
 var batterieprix=15;
 var total;
 controleur =Number(prompt("choisissez le controleur que vous voulez tapez 1 pour le raspberry pie à 55$ ou 2 pour le raspberry pi zero" ));
-moteurs= Number(prompt("combien de moteurs avez vous besoin choissisez entre 1 et 24"));
+moteurs= Number(prompt("combien de moteurs avez vous besoin choissisez entre 2 et 24"));
 camera = Number(prompt("voulez vous une camera USB a 35$? tapez 1 pour oui ou 2 pour non"));
 LED=Number(prompt("voulez vous une matrice de LED pour les yeux à 10$ tapez 1 pour oui ou 2 pour non"));
 
@@ -20,7 +20,7 @@ if(controleur === 1){
     document.write("raspberry pi");
     document.write("<br/>")
 }
-    else if(controleur=== 2){
+    else if(controleur === 2){
         controleurprix=15;
     document.write("raspberry pi zero");
     document.write("<br/>")
@@ -39,12 +39,12 @@ if(camera === 1){
     else if(camera === 2){
         cameraprix=0
 }
-if(LED.toLowerCase() === 1){
+if(LED === 1){
     LEDprix=10;
     document.write("LED");
     document.write("<br/>")
 }
-    else if(LED ===  2){
+    else if(LED === 2){
         LEDprix=0
 }
 document.write("filament3D");
