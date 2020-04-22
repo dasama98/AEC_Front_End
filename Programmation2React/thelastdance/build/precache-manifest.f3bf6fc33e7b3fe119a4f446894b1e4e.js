728ffeae45499d75bbcd3bffa6c187d34eed146b
dasama98 <davidgendron@hotmail.com>
2020-04-21 21:02:40 -0400

    second react commit

---
+++ Programmation2React/thelastdance/build/precache-manifest.f3bf6fc33e7b3fe119a4f446894b1e4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003e2af4115324e803ee575754d6dedf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9dbd503b34a5a9f56a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "acba0160f3b65756036f",
+    "url": "/static/js/2.8f6e9f04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f6e9f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9dbd503b34a5a9f56a",
+    "url": "/static/js/main.b9dd74b0.chunk.js"
+  },
+  {
+    "revision": "914341b2d8fa5e1625b4",
+    "url": "/static/js/runtime-main.a86621e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
